bedb53ad40fe49066a80fb72d0e789071bb42370
John Alvaro <jhon54plex@hotmail.com>
2018-10-02 16:31:12 -0500

    Inicio correccion responsive

--- resources/views/tienda.blade.php
+++ resources/views/tienda.blade.php
@@ -64,7 +64,7 @@
 
     <a v-for="producto in productos" v-bind:href="'detalle/'+producto.id">
     <div class="col-md-4 col-xs-6 " id="general" >
-    <img class="img-productos" v-bind:src="producto.imagen" alt="">
+    <img class="img-productos" v-bind:src="'/'+producto.imagen" alt="">
     <div class="caja-text-producto">
     <p class="text-categoria text-categoria2">@{{producto.nombre}} <br>  $@{{producto.precio}}</p>
     </div>
